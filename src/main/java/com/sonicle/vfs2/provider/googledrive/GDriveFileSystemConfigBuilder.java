cf6b2c1d1d0a7f5473ed8f4c74a48800503e0784
Matteo Albinola <malbinola@sonicle.com>
2016-11-11 11:23:59 +0100

    Update license

--- src/main/java/com/sonicle/vfs2/provider/googledrive/GDriveFileSystemConfigBuilder.java
+++ src/main/java/com/sonicle/vfs2/provider/googledrive/GDriveFileSystemConfigBuilder.java
@@ -1,5 +1,4 @@
-/*
- * cloud-services is a library developed by Sonicle S.r.l.
+/* 
  * Copyright (C) 2014 Sonicle S.r.l.
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -11,7 +10,7 @@
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Affero General Public License
@@ -19,7 +18,7 @@
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301 USA.
  *
- * You can contact Sonicle S.r.l. at email address sonicle@sonicle.com
+ * You can contact Sonicle S.r.l. at email address sonicle[at]sonicle.com
  *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
@@ -31,7 +30,6 @@
  * reasonably feasible for technical reasons, the Appropriate Legal Notices must
  * display the words "Copyright (C) 2014 Sonicle S.r.l.".
  */
-
 package com.sonicle.vfs2.provider.googledrive;
 
 import org.apache.commons.vfs2.FileSystem;
